f6bc7148fb4df1ba8fc5a6be75c62ffa8735846c
Iankidd <iankidd@vip.qq.com>
2018-09-03 21:48:04 +0800

    重构后台模板页面(干净)

--- service-oss/src/main/resources/static/js/inspinia.js
+++ service-oss/src/main/resources/static/js/inspinia.js
@@ -1,29 +1,37 @@
 /*
  *
  *   INSPINIA - Responsive Admin Theme
- *   version 2.2
+ *   version 2.8
  *
  */
 
-
 $(document).ready(function () {
+
+    // Fast fix bor position issue with Propper.js
+    // Will be fixed in Bootstrap 4.1 - https://github.com/twbs/bootstrap/pull/24092
+    Popper.Defaults.modifiers.computeStyle.gpuAcceleration = false;
 
 
     // Add body-small class if window less than 768px
-    if ($(this).width() < 769) {
+    if ($(window).width() < 769) {
         $('body').addClass('body-small')
     } else {
         $('body').removeClass('body-small')
     }
 
-    // MetsiMenu
-    $('#side-menu').metisMenu();
+    // MetisMenu
+    var sideMenu = $('#side-menu').metisMenu();
+
+    sideMenu.on('shown.metisMenu', function (e) {
+        fix_height();
+    });
 
     // Collapse ibox function
-    $('.collapse-link').click(function () {
+    $('.collapse-link').on('click', function (e) {
+        e.preventDefault();
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
-        var content = ibox.find('div.ibox-content');
+        var content = ibox.children('.ibox-content');
         content.slideToggle(200);
         button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
         ibox.toggleClass('').toggleClass('border-bottom');
@@ -34,31 +42,41 @@ $(document).ready(function () {
     });
 
     // Close ibox function
-    $('.close-link').click(function () {
+    $('.close-link').on('click', function (e) {
+        e.preventDefault();
         var content = $(this).closest('div.ibox');
         content.remove();
     });
 
     // Fullscreen ibox function
-    $('.fullscreen-link').click(function() {
+    $('.fullscreen-link').on('click', function (e) {
+        e.preventDefault();
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
         $('body').toggleClass('fullscreen-ibox-mode');
         button.toggleClass('fa-expand').toggleClass('fa-compress');
         ibox.toggleClass('fullscreen');
-        setTimeout(function() {
+        setTimeout(function () {
             $(window).trigger('resize');
         }, 100);
     });
 
     // Close menu in canvas mode
-    $('.close-canvas-menu').click(function () {
+    $('.close-canvas-menu').on('click', function (e) {
+        e.preventDefault();
         $("body").toggleClass("mini-navbar");
         SmoothlyMenu();
     });
 
+    // Run menu of canvas
+    $('body.canvas-menu .sidebar-collapse').slimScroll({
+        height: '100%',
+        railOpacity: 0.9
+    });
+
     // Open close right sidebar
-    $('.right-sidebar-toggle').click(function () {
+    $('.right-sidebar-toggle').on('click', function (e) {
+        e.preventDefault();
         $('#right-sidebar').toggleClass('sidebar-open');
     });
 
@@ -70,8 +88,9 @@ $(document).ready(function () {
     });
 
     // Open close small chat
-    $('.open-small-chat').click(function () {
-        $(this).children().toggleClass('fa-comments').toggleClass('fa-remove');
+    $('.open-small-chat').on('click', function (e) {
+        e.preventDefault();
+        $(this).children().toggleClass('fa-comments').toggleClass('fa-times');
         $('.small-chat-box').toggleClass('active');
     });
 
@@ -82,7 +101,7 @@ $(document).ready(function () {
     });
 
     // Small todo handler
-    $('.check-link').click(function () {
+    $('.check-link').on('click', function () {
         var button = $(this).find('i');
         var label = $(this).next('span');
         button.toggleClass('fa-check-square').toggleClass('fa-square-o');
@@ -90,15 +109,9 @@ $(document).ready(function () {
         return false;
     });
 
-    // Append config box / Only for demo purpose
-    // Uncomment on server mode to enable XHR calls
-    $.get("skin-config.html", function (data) {
-        if (!$('body').hasClass('no-skin-config'))
-            $('body').append(data);
-    });
-
     // Minimalize menu
-    $('.navbar-minimalize').click(function () {
+    $('.navbar-minimalize').on('click', function (event) {
+        event.preventDefault();
         $("body").toggleClass("mini-navbar");
         SmoothlyMenu();
 
@@ -110,43 +123,6 @@ $(document).ready(function () {
         container: "body"
     });
 
-    // Move modal to body
-    // Fix Bootstrap backdrop issu with animation.css
-    $('.modal').appendTo("body");
-
-    // Full height of sidebar
-    function fix_height() {
-        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
-        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
-
-        var navbarHeigh = $('nav.navbar-default').height();
-        var wrapperHeigh = $('#page-wrapper').height();
-
-        if (navbarHeigh > wrapperHeigh) {
-            $('#page-wrapper').css("min-height", navbarHeigh + "px");
-        }
-
-        if (navbarHeigh < wrapperHeigh) {
-            $('#page-wrapper').css("min-height", $(window).height() + "px");
-        }
-
-        if ($('body').hasClass('fixed-nav')) {
-            $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
-        }
-
-    }
-
-    fix_height();
-
-    // Fixed Sidebar
-    $(window).bind("load", function () {
-        if ($("body").hasClass('fixed-sidebar')) {
-            $('.sidebar-collapse').slimScroll({
-                height: '100%',
-                railOpacity: 0.9
-            });
-        }
-    });
 
     // Move right sidebar top after scroll
     $(window).scroll(function () {
@@ -154,12 +130,6 @@ $(document).ready(function () {
             $('#right-sidebar').addClass('sidebar-top');
         } else {
             $('#right-sidebar').removeClass('sidebar-top');
-        }
-    });
-
-    $(window).bind("load resize scroll", function () {
-        if (!$("body").hasClass('body-small')) {
-            fix_height();
         }
     });
 
@@ -173,6 +143,52 @@ $(document).ready(function () {
 });
 
 
+// Fixed Sidebar
+$(window).bind("load", function () {
+    if ($("body").hasClass('fixed-sidebar')) {
+        $('.sidebar-collapse').slimScroll({
+            height: '100%',
+            railOpacity: 0.9
+        });
+    }
+});
+
+function fix_height() {
+    var heightWithoutNavbar = $("body > #wrapper").height() - 62;
+    $(".sidebar-panel").css("min-height", heightWithoutNavbar + "px");
+
+    var navbarheight = $('nav.navbar-default').height();
+    var wrapperHeight = $('#page-wrapper').height();
+
+    if (navbarheight > wrapperHeight) {
+        $('#page-wrapper').css("min-height", navbarheight + "px");
+    }
+
+    if (navbarheight < wrapperHeight) {
+        $('#page-wrapper').css("min-height", $(window).height() + "px");
+    }
+
+    if ($('body').hasClass('fixed-nav')) {
+        if (navbarheight > wrapperHeight) {
+            $('#page-wrapper').css("min-height", navbarheight + "px");
+        } else {
+            $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+        }
+    }
+
+}
+
+$(window).bind("load resize scroll", function () {
+
+    // Full height of sidebar
+    setTimeout(function () {
+        if (!$("body").hasClass('body-small')) {
+            fix_height();
+        }
+    })
+
+});
+
 // Minimalize menu when screen is less than 768px
 $(window).bind("resize", function () {
     if ($(this).width() < 769) {
@@ -185,7 +201,7 @@ $(window).bind("resize", function () {
 // Local Storage functions
 // Set proper body class and plugins based on user configuration
 $(document).ready(function () {
-    if (localStorageSupport) {
+    if (localStorageSupport()) {
 
         var collapse = localStorage.getItem("collapse_menu");
         var fixedsidebar = localStorage.getItem("fixedsidebar");
@@ -258,14 +274,14 @@ function SmoothlyMenu() {
         // For smoothly turn on menu
         setTimeout(
             function () {
-                $('#side-menu').fadeIn(500);
-            }, 100);
+                $('#side-menu').fadeIn(400);
+            }, 200);
     } else if ($('body').hasClass('fixed-sidebar')) {
         $('#side-menu').hide();
         setTimeout(
             function () {
-                $('#side-menu').fadeIn(500);
-            }, 300);
+                $('#side-menu').fadeIn(400);
+            }, 100);
     } else {
         // Remove all inline style from jquery fadeIn function to reset menu state
         $('#side-menu').removeAttr('style');
